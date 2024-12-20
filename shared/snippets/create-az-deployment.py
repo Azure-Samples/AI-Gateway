2522307adae96544343e1ae4263765c01245a5ab
Simon Kurtz <simonkurtz@microsoft.com>
2024-12-20 09:48:58 -0500

    Add code snippets

---
+++ shared/snippets/create-az-deployment.py
@@ -0,0 +1,25 @@
+import json
+
+backend_id = "openai-backend-pool" if len(openai_resources) > 1 else openai_resources[0].get("name")
+
+with open("policy.xml", 'r') as policy_xml_file:
+    policy_template_xml = policy_xml_file.read()
+    policy_xml = policy_template_xml.replace("{backend-id}", backend_id)
+    policy_xml_file.close()
+open("policy-updated.xml", 'w').write(policy_xml)
+
+bicep_parameters = {
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "openAIConfig": { "value": openai_resources },
+        "openAIDeploymentName": { "value": openai_deployment_name },
+        "openAIModelName": { "value": openai_model_name },
+        "openAIModelVersion": { "value": openai_model_version },
+    }
+}
+
+with open('params.json', 'w') as bicep_parameters_file:
+    bicep_parameters_file.write(json.dumps(bicep_parameters))
+
+! az deployment group create --name {deployment_name} --resource-group {resource_group_name} --template-file "main.bicep" --parameters "params.json"
