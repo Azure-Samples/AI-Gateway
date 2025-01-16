5ffbdd845c6c39c79f717d0449df5eedf6f04b4a
Simon Kurtz <simonkurtz@microsoft.com>
2025-01-16 13:05:48 -0500

    Clean-up and organization of utils

--- shared/utils.py
+++ shared/utils.py
@@ -1,75 +1,28 @@
-import os, subprocess, datetime, time, json, traceback
+import datetime, json, os, subprocess, time, traceback
 
-print_info = lambda message: print("👉🏽 \x1b[1;34m", message, "\x1b[0m")
-print_message = lambda message, output='', duration='': print("👉🏽 \x1b[1;32m", message, "\x1b[0m⌚", datetime.datetime.now().time(), duration, "\n" if output else "", output)
-print_ok = lambda message, output='', duration='': print("✅ \x1b[1;32m", message, "\x1b[0m⌚", datetime.datetime.now().time(), duration, "\n" if output else "", output)
-print_error = lambda message, output='', duration='': print("⛔ \x1b[1;31m", message, "\x1b[0m⌚", datetime.datetime.now().time(), duration, "\n" if output else "", output)
-print_warning = lambda message, output='', duration='': print("⚠️ \x1b[1;33m", message, "\x1b[0m⌚", datetime.datetime.now().time(), duration, "\n" if output else "", output)
-print_command = lambda command ='': print("⚙️ \x1b[1;34m Running: ", command, "\x1b[0m")
+# Define ANSI escape code constants vor clarity in the print commands below
+RESET_FORMATTING = "\x1b[0m"
+BOLD_BLUE = "\x1b[1;34m"
+BOLD_RED = "\x1b[1;31m"
+BOLD_GREEN = "\x1b[1;32m"
+BOLD_YELLOW = "\x1b[1;33m"
+
+print_command = lambda command='': print(f"⚙️ {BOLD_BLUE}Running: {command} {RESET_FORMATTING}")
+print_error = lambda message, output='', duration='': print(f"⛔ {BOLD_RED}{message}{RESET_FORMATTING} ⌚ {datetime.datetime.now().time()} {duration}{' ' if output else ''}{output}")
+print_info = lambda message: print(f"👉🏽 {BOLD_BLUE}{message}{RESET_FORMATTING}")
+print_message = lambda message, output='', duration='': print(f"👉🏽 {BOLD_GREEN}{message}{RESET_FORMATTING} ⌚ {datetime.datetime.now().time()} {duration}{' ' if output else ''}{output}")
+print_ok = lambda message, output='', duration='': print(f"✅ {BOLD_GREEN}{message}{RESET_FORMATTING} ⌚ {datetime.datetime.now().time()} {duration}{' ' if output else ''}{output}")
+print_warning = lambda message, output='', duration='': print(f"⚠️ {BOLD_YELLOW}{message}{RESET_FORMATTING} ⌚ {datetime.datetime.now().time()} {duration}{' ' if output else ''}{output}")
+
 class Output(object):
     def __init__(self, success, text):
         self.success = success
         self.text = text
+
         try:
             self.json_data = json.loads(text)
         except:
             self.json_data = None
-def run(command, ok_message = '', error_message = '', print_output = False, print_command_to_run = True):
-    if print_command_to_run:
-        print_command(command)
-    start_time = time.time()
-    try:
-        output_text = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode("utf-8")
-        success = True
-    except subprocess.CalledProcessError as e:
-        output_text = e.output.decode("utf-8")
-        success = False
-    minutes, seconds = divmod(time.time() - start_time, 60)
-    print_message = print_ok if success else print_error
-    if (ok_message or error_message):
-        print_message(ok_message if success else error_message, output_text if not success or print_output  else "", f"[{int(minutes)}m:{int(seconds)}s]")
-    return Output(success, output_text)
-
-def create_resource_group(create_resources, resource_group_name, resource_group_location):
-    if not resource_group_name:
-        print_warning('Please specify the resource group name')
-    else:
-        output = run(f"az group show --name {resource_group_name}")
-        if create_resources:    
-            if output.success:
-                print_info(f"Using existing resource group '{resource_group_name}'")
-            else:
-                output = run(f"az group create --name {resource_group_name} --location {resource_group_location}", 
-                                        f"Resource group '{resource_group_name}' created", 
-                                        f"Failed to create the resource group '{resource_group_name}'")
-        else:
-            if output.success:
-                print_info(f"Using resource group '{resource_group_name}'")
-            else:
-                print_error(f"Resource group '{resource_group_name}' does not exist")
-
-def get_deployment_output(output, output_property, output_label = '', secure = False):
-    try:       
-        deployment_output = output.json_data['properties']['outputs'][output_property]['value'] 
-        if output_label:
-            if secure:
-                print_info(f"{output_label}: ****{deployment_output[-4:]}")
-            else:
-                print_info(f"{output_label}: {deployment_output}")
-        return deployment_output
-    except:
-        print_error(f"Failed to retieve output property: '{output_property}'")
-        return None
-    
-def print_response(response):
-    print("Response headers: ", response.headers)
-    if (response.status_code == 200):
-        print_ok(f"Status Code: {response.status_code}")
-        data = json.loads(response.text)
-        print(json.dumps(data, indent=4))
-    else:
-        print_warning(f"Status Code: {response.status_code}")
-        print(response.text)
 
 # Cleans up resources associated with a deployment in a resource group
 def cleanup_resources(deployment_name, resource_group_name = None):
@@ -85,8 +38,10 @@ def cleanup_resources(deployment_name, resource_group_name = None):
 
         # Show the deployment details
         output = run(f"az deployment group show --name {deployment_name} -g {resource_group_name} -o json", "Deployment retrieved", "Failed to retrieve the deployment")
+
         if output.success and output.json_data:
             resources = output.json_data.get("properties", {}).get("outputResources", [])
+
             if resources is None:
                 print_error(f"No resources found for deployment '{deployment_name}'.")
                 return
@@ -101,6 +56,7 @@ def cleanup_resources(deployment_name, resource_group_name = None):
 
                     try:
                         output = run(f"az resource show --id {resource_id} --query \"{{type:type, name:name, location:location}}\" -o json")
+
                         if output.success and output.json_data:
                             delete_resource(output.json_data, resource_group_name)
 
@@ -117,13 +73,31 @@ def cleanup_resources(deployment_name, resource_group_name = None):
         print(f"An error occurred during cleanup: {e}")
         traceback.print_exc()
 
+def create_resource_group(resource_group_name, resource_group_location = None):
+    if not resource_group_name:
+        print_error('Please specify the resource group name.')
+    else:
+        output = run(f"az group show --name {resource_group_name}")
+
+        if output.success:
+            print_info(f"Using existing resource group '{resource_group_name}'")
+        else:
+            if not resource_group_location:
+                print_error('Please specify the resource group location.')
+            else:
+                print_info(f"Resource group {resource_group_name} does not yet exist. Creating the resource group now...")
+
+                output = run(f"az group create --name {resource_group_name} --location {resource_group_location}",
+                    f"Resource group '{resource_group_name}' created",
+                    f"Failed to create the resource group '{resource_group_name}'")
+
 # Deletes a specific resource based on its type
 def delete_resource(resource, resource_group_name):
     resource_name = resource.get("name")
     resource_type = resource.get("type")
     resource_location = resource.get("location")
 
-    print(f"🗑  Deleting {resource_type} '{resource_name}' in resource group '{resource_group_name}'...")
+    print(f"🗑 Deleting {resource_type} '{resource_name}' in resource group '{resource_group_name}'...")
 
     # API Management
     if resource_type == "Microsoft.ApiManagement/service":
@@ -141,7 +115,63 @@ def delete_resource(resource, resource_group_name):
     elif resource_type == "Microsoft.KeyVault/vaults":
         output = run(f"az keyvault delete -n {resource_name} -g {resource_group_name}", f"Key Vault '{resource_name}' deleted", f"Failed to delete Key Vault '{resource_name}'")
 
+def get_deployment_output(output, output_property, output_label = '', secure = False) -> str:
+    try:
+        deployment_output = output.json_data['properties']['outputs'][output_property]['value']
 
+        if output_label:
+            if secure:
+                print_info(f"{output_label}: ****{deployment_output[-4:]}")
+            else:
+                print_info(f"{output_label}: {deployment_output}")
 
+        return str(deployment_output)
+    except Exception as e:
+        error = f"Failed to retrieve output property: '{output_property}'\nError: {e}"
+        print_error(error)
+        raise Exception(error)
 
+def print_response(response):
+    print("Response headers: ", response.headers)
 
+    if (response.status_code == 200):
+        print_ok(f"Status Code: {response.status_code}")
+        data = json.loads(response.text)
+        print(json.dumps(data, indent=4))
+    else:
+        print_warning(f"Status Code: {response.status_code}")
+        print(response.text)
+
+def print_response_code(response):
+    # Check the response status code and apply formatting
+    if 200 <= response.status_code < 300:
+        status_code_str = f"{BOLD_GREEN}{response.status_code} - {response.reason}{RESET_FORMATTING}"
+    elif response.status_code >= 400:
+        status_code_str = f"{BOLD_RED}{response.status_code} - {response.reason}{RESET_FORMATTING}"
+    else:
+        status_code_str = str(response.status_code)
+
+    # Print the response status with the appropriate formatting
+    print(f"Response status: {status_code_str}")
+
+def run(command, ok_message = '', error_message = '', print_output = False, print_command_to_run = True):
+    if print_command_to_run:
+        print_command(command)
+
+    start_time = time.time()
+
+    try:
+        output_text = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode("utf-8")
+        success = True
+    except subprocess.CalledProcessError as e:
+        output_text = e.output.decode("utf-8")
+        success = False
+
+    minutes, seconds = divmod(time.time() - start_time, 60)
+
+    print_message = print_ok if success else print_error
+
+    if (ok_message or error_message):
+        print_message(ok_message if success else error_message, output_text if not success or print_output  else "", f"[{int(minutes)}m:{int(seconds)}s]")
+
+    return Output(success, output_text)
