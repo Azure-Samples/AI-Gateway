1559b7d9fb6bb288cc7091d2e9452bd0fe2ce8ed
Houssem Dellai <hodellai@microsoft.com>
2025-04-10 18:05:18 +0200

    work in progress

--- labs/backend-pool-load-balancing-tf/main.tf
+++ labs/backend-pool-load-balancing-tf/main.tf
@@ -68,21 +68,6 @@ resource "azapi_resource" "apim" {
   response_export_values = ["*"]
 }
 
-# resource "azurerm_api_management" "apim" {
-#   name                          = "apim-genai-${var.prefix}"
-#   location                      = azurerm_resource_group.rg.location
-#   resource_group_name           = azurerm_resource_group.rg.name
-#   publisher_name                = "My Company"
-#   publisher_email               = "noreply@microsoft.com"
-#   sku_name                      = "Consumption_0" # "Developer_1"
-#   virtual_network_type          = "None"          # None, External, Internal
-#   public_network_access_enabled = true            # false applies only when using private endpoint as the exclusive access method
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
-
 resource "azurerm_role_assignment" "Cognitive-Services-OpenAI-User" {
   for_each = var.openai_config
 
